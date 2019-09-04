1ca53d992f5d8a54fd3f962e3c19201d39e25fda
Marco Meisen <marcomeisen@gmail.com>
2019-09-04 15:14:31 +0200

    Platform Picker (Refactored)

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -51,14 +51,6 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
-  Widget getPicker() {
-    if (Platform.isIOS) {
-      return iOSPicker();
-    } else if (Platform.isAndroid) {
-      return androidDropdown();
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -95,7 +87,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: getPicker(),
+            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
           ),
         ],
       ),
